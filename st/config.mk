3c77136c99ec72357dbb5d5d6de212e825a52992
nkoehring <n@koehr.in>
2016-05-20 01:54:21 +0200

    update

---
+++ st/config.mk
@@ -0,0 +1,28 @@
+# st version
+VERSION = 0.6
+
+# Customize below to fit your system
+
+# paths
+PREFIX = ~/.local
+MANPREFIX = ${PREFIX}/share/man
+
+X11INC = /usr/include/X11
+X11LIB = /usr/lib
+
+# includes and libs
+INCS = -I. -I/usr/include -I${X11INC} \
+       `pkg-config --cflags fontconfig` \
+       `pkg-config --cflags freetype2`
+LIBS = -L/usr/lib -lc -L${X11LIB} -lm -lrt -lX11 -lutil -lXft \
+       `pkg-config --libs fontconfig`  \
+       `pkg-config --libs freetype2`
+
+# flags
+CPPFLAGS = -DVERSION=\"${VERSION}\" -D_XOPEN_SOURCE=600
+CFLAGS += -g -std=c99 -pedantic -Wall -Wvariadic-macros -Os ${INCS} ${CPPFLAGS}
+LDFLAGS += -g ${LIBS}
+
+# compiler and linker
+# CC = cc
+
